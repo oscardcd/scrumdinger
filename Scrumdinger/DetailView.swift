955a63197f8c33a5055d3a8d960a505b86df9b7a
oscardcd <oscardcd17@gmail.com>
2023-12-20 19:53:13 -0500

    add edit detail sheet to edit details view to update the meet information

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DetailView: View {
     
     let scrum: DailyScrum
+    @State private var isPresentingEditView = false
     
     var body: some View {
         List{
@@ -44,6 +45,25 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar{
+            Button("Edit"){
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar{
+                        ToolbarItem(placement: .cancellationAction){
+                            Button("Cancel"){
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+                    
+            }
+        }
     }
 }
 
